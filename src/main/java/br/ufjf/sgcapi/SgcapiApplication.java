e77544822e64e7dbea5608f5f646244841c21b12
Bernardo <bressan.mattos@estudante.ufjf.br>
2026-04-28 21:54:48 -0300

    Initial Commit

---
+++ src/main/java/br/ufjf/sgcapi/SgcapiApplication.java
@@ -0,0 +1,13 @@
+package br.ufjf.sgcapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgcapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgcapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ufjf/sgcapi/SgcapiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ufjf.sgcapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgcapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
