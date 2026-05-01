f3cf65c0135ce86bf4d30e34ec55ab8571d07c0d
Pedro Bonfá <pedro.bonfa@estudante.ufjf.br>
2026-04-30 22:11:29 -0300

    fix TestDrive e CarroDeTestDrive

--- src/main/java/br/ufjf/sgcapi/model/entity/TestDrive.java
+++ src/main/java/br/ufjf/sgcapi/model/entity/TestDrive.java
@@ -1,15 +1,23 @@
 package br.ufjf.sgcapi.model.entity;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 
 public class TestDrive {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
     private Cliente cliente;
 
+    @ManyToOne
     private Funcionario funcionario;
 }
